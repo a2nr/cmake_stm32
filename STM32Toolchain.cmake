e1032549bfd751520134ebbfea9a7abba7c60d62
Anggoro Dwi <borni3s.burn@gmail.com>
2018-06-14 22:21:49 +0700

    debug on ram for development process

--- STM32Toolchain.cmake
+++ STM32Toolchain.cmake
@@ -1,4 +1,3 @@
-include(CMakeForceCompiler)
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
@@ -15,10 +14,8 @@ set(CMAKE_LINKER                ${COMPILER}-ld)
 set(CMAKE_ASM_COMPILER          ${COMPILER}-as)
 set(CMAKE_SIZE                  ${COMPILER}-size)
 
-
-set(COMMON_FLAGS "-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -lc -specs=nosys.specs -mthumb -mthumb-interwork -ffunction-sections -fdata-sections -g -fno-common -fmessage-length=0")
+set(COMMON_FLAGS "-g3 -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -lc --specs=nosys.specs -mthumb -mthumb-interwork -ffunction-sections -fdata-sections -g -fno-common -fmessage-length=0")
 
 set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=c++11")
 set(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu99")
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections -T ${CMAKE_SOURCE_DIR}/STM32F446RETx_FLASH.ld")
-# -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections -T")
